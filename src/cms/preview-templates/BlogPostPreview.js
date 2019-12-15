270f726db4e5f561594caef831b8a99f37a9ecd4
Tony Martinez <ajosephmartinez@gmail.com>
2019-12-15 10:13:04 -0800

    .

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../../templates/blog-post'
+import React from "react";
+import PropTypes from "prop-types";
+import { BlogPostTemplate } from "../../templates/blog-post";
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
-)
+// const BlogPostPreview = ({ entry, widgetFor }) => (
+//   <BlogPostTemplate
+//     content={widgetFor('body')}
+//     description={entry.getIn(['data', 'description'])}
+//     tags={entry.getIn(['data', 'tags'])}
+//     title={entry.getIn(['data', 'title'])}
+//   />
+// )
 
-BlogPostPreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
-}
+// BlogPostPreview.propTypes = {
+//   entry: PropTypes.shape({
+//     getIn: PropTypes.func,
+//   }),
+//   widgetFor: PropTypes.func,
+// }
 
-export default BlogPostPreview
+const BlogPostPreview = props => <div>Preview</div>;
+
+export default BlogPostPreview;
