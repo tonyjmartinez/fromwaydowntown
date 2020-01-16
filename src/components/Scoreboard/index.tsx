d9d28f6df40810574e6b4d24e8928133cb1f548c
Tony Martinez <ajosephmartinez@gmail.com>
2020-01-15 20:54:15 -0800

    .

--- src/components/Scoreboard/index.tsx
+++ src/components/Scoreboard/index.tsx
@@ -7,6 +7,7 @@ import styled from "styled-components";
 import axios from "axios";
 import _ from "lodash";
 import teams from "../../data/teams";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import * as logos from "react-nba-logos";
 
 const ContentWrapper = styled.div`
@@ -20,12 +21,20 @@ const getNumberWithOrdinal = n => {
 };
 const findTeam = (teams, teamId) =>
   _.find(teams, team => team.teamId === teamId);
+const large = useMediaQuery("(min-width:900pix)");
+const medium = useMediaQuery("(min-width:");
 
 const getGameState = game => {
   const { isGameActivated, period, clock, startTimeEastern, statusNum } = game;
+  const currentPeriod = getNumberWithOrdinal(period.current);
   if (isGameActivated) {
     if (period.isHalftime) return "Halftime";
-    return `${clock} ${getNumberWithOrdinal(period.current)}`;
+    if (period.isEndOfPeriod) return `End of the ${currentPeriod}`;
+    if (statusNum === 1) return `Game Start`;
+    if (period.current > 4) {
+      return period.current === 5 ? "OT" : `${period.current - 4} OT`;
+    }
+    return `${clock} ${currentPeriod}`;
   }
   if (statusNum === 1) {
     return startTimeEastern;
@@ -37,52 +46,54 @@ const getGameState = game => {
 
 const Scoreboard = props => {
   const [teamScores, setTeamScores] = useState([]);
-  useEffect(() => {
-    const setScores = games => {
-      const scores = games.map(game => {
-        const { hTeam, vTeam, startTimeEastern } = game;
-        const homeTeamInfo = findTeam(teams, hTeam.teamId);
-        const visitingTeamInfo = findTeam(teams, vTeam.teamId);
-        const HomeLogo = logos[homeTeamInfo.tricode];
-        const VisitingLogo = logos[visitingTeamInfo.tricode];
-        console.log("getgamestate", getGameState(game));
-        return {
-          gameState: getGameState(game),
-          home: {
-            score: hTeam.score,
-            name: `${homeTeamInfo.tricode} ${homeTeamInfo.nickname}`,
-            logo: <HomeLogo size={30} />
-          },
-          visitor: {
-            score: vTeam.score,
-            name: `${visitingTeamInfo.tricode} ${visitingTeamInfo.nickname}`,
-            logo: <VisitingLogo size={30} />
-          }
-        };
-      });
-      console.log(scores);
-      setTeamScores(scores);
-    };
+  let width = "80%";
+  if (large)
+    useEffect(() => {
+      const setScores = games => {
+        const scores = games.map(game => {
+          const { hTeam, vTeam, startTimeEastern } = game;
+          const homeTeamInfo = findTeam(teams, hTeam.teamId);
+          const visitingTeamInfo = findTeam(teams, vTeam.teamId);
+          const HomeLogo = logos[homeTeamInfo.tricode];
+          const VisitingLogo = logos[visitingTeamInfo.tricode];
+          console.log("getgamestate", getGameState(game));
+          return {
+            gameState: getGameState(game),
+            home: {
+              score: hTeam.score,
+              name: `${homeTeamInfo.tricode} ${homeTeamInfo.nickname}`,
+              logo: <HomeLogo size={30} />
+            },
+            visitor: {
+              score: vTeam.score,
+              name: `${visitingTeamInfo.tricode} ${visitingTeamInfo.nickname}`,
+              logo: <VisitingLogo size={30} />
+            }
+          };
+        });
+        console.log(scores);
+        setTeamScores(scores);
+      };
 
-    const fetchGames = async () => {
-      const fmtDate = moment().format("YYYYMMDD");
-      const res = await axios.get(`/.netlify/functions/nba?date=${fmtDate}`);
-      console.log(res.data);
-      const games = _.get(res, "data.games.games");
-      console.log("games?", games);
+      const fetchGames = async () => {
+        const fmtDate = moment().format("YYYYMMDD");
+        const res = await axios.get(`/.netlify/functions/nba?date=${fmtDate}`);
+        console.log(res.data);
+        const games = _.get(res, "data.games.games");
+        console.log("games?", games);
 
-      games && setScores(games);
-      console.log(games);
-      setTimeout(fetchGames, 20000);
-    };
-    fetchGames();
-  }, []);
+        games && setScores(games);
+        console.log(games);
+        setTimeout(fetchGames, 20000);
+      };
+      fetchGames();
+    }, []);
 
   console.log("scores??", teamScores);
   return (
     <Layout>
       <ContentWrapper style={{ width: "80%" }}>
-        <Card scores={teamScores} />
+        <Card cardStyle={{}} scores={teamScores} />
       </ContentWrapper>
     </Layout>
   );
